c183743ab685666c7db9b352b58ccf5777879b96
Travis J. Webb <me@traviswebb.com>
2016-12-06 02:11:57 -0500

    [api] remove all trails/trailpack dependency

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 exports.Logger = require('./lib/logger')
-exports.Trailpack = require('./lib/trailpack')
 exports.FailsafeConfig = require('./lib/failsafeconfig')
 exports.Ecosystem = require('./lib/ecosystem')
-exports.TrailsApp = require('./lib/trails')
